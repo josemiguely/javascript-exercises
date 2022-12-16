fc6607289294606fc361d20086b25dccb6ee1884
Jose Miguel <josemiguelyuseff@gmail.com>
2022-12-16 12:22:58 -0300

    First two exercises completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let rest="";
+    for (let i = string.length-1;i>-1;i--){
+        rest+=string[i];
+    }
+    return rest;
 };
 
 // Do not edit below this line
